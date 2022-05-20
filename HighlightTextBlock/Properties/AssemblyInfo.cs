5b961d0217fa8e47b1a7b0d96dc0dece5d527666
Platonenkov <Platonenkov87@gmail.com>
2022-05-20 14:01:00 +0300

    net6 support

---
+++ HighlightTextBlock/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Markup;
+[assembly: XmlnsPrefix("clr-namespace:HighlightTextBlock;assembly=HighlightTextBlock", "hltb")]
+[assembly: XmlnsDefinition("clr-namespace:HighlightTextBlock;assembly=HighlightTextBlock", "HighlightTextBlock")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
